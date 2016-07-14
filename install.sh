958d692136eed339f9be66a40c0cbdc1e9d3ab5d
Viktor <mail@viktor-schlueter.de>
2016-07-14 16:36:51 +0200

    pathogen

--- install.sh
+++ install.sh
@@ -5,5 +5,8 @@
 cp -rf .vim ~/.vim
 cp -f .vimrc ~/.vimrc
 
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
 sudo apt-get install tmux
 
